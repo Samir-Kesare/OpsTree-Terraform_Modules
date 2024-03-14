5bfad7a449de12b22c6083396fb2f5d613ec380b
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-03-14 11:30:34 +0530

    Create variables.tf

---
+++ VM-Module/variables.tf
@@ -0,0 +1,58 @@
+variable "sec_grp_name" {
+  description     = "Name for the security group"
+  type            = string
+  default         = "dev_sg"
+}
+variable "Sec_grp_description" {
+  description     = "Description for the security group"
+  type            = string
+  default         = "Security group for Dev Env"
+}
+variable "vpc_id" {
+  description     = "ID of the VPC for instances"
+  type            = string
+}
+
+variable "inbound_ports" {
+  description     = "List of inbound ports, protocols and cidr block"
+  type            = list(map(any))
+  default         = [
+    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, 
+  ]
+}
+
+variable "outbound_ports" {
+  description     = "List of outbound ports, protocols and Cidr block "
+  type            = list(map(any))
+  default         = [
+    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
+  ]
+}
+variable "Sec_grp_tags" {
+  type            = map(string)
+  default         = {
+    Name          = "dev-sg"
+    Enviroment    = "dev"
+    Owner         = "shreya"
+  }
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "server_type" {
+  description = "Instance type for Standalone server"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet for the Standalone server"
+  type        = string
+}
+
+variable "server_name" {
+  description = "Name tag for the standalone server"
+  type        = string
+}
