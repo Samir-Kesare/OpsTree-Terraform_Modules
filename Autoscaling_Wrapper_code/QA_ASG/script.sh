21400998743dc28068d5d24b3564398f2b047227
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-14 13:59:49 +0530

    Update and rename script.sh to script.sh

--- Autoscaling_Wrapper_code/QA_ASG/script.sh
+++ Autoscaling_Wrapper_code/QA_ASG/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALB_DNS="Dev-ALB-394440977.ap-northeast-1.elb.amazonaws.com"  # Change the DNS give actual DEV-ALB DNS
+ALB_DNS="QA-ALB-1428988738.ap-northeast-1.elb.amazonaws.com"
 # Modify package.json with the ALB DNS placeholder
 sed -i 's/"homepage": "https:\/\/opstree.github.io",/"homepage": "http:\/\/${ALB_DNS}",/' /home/ubuntu/Frontend/package.json
 sed -i '/"proxy": "http:\/\/localhost:3000"/d' /home/ubuntu/Frontend/package.json
