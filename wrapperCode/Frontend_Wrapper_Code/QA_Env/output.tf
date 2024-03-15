b2b400dc936460c910b025aba5fb63d27b6f7247
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-15 11:59:09 +0530

    Update output.tf

--- wrapperCode/Frontend_Wrapper_Code/QA_Env/output.tf
+++ wrapperCode/Frontend_Wrapper_Code/QA_Env/output.tf
@@ -1,7 +1,7 @@
 #---------------------------------Security Group ----------------------------------#
 
 output "Security_Group_ID" {
-  value = [module.ASG.Security_Group_ID]
+  value = [module.QA_Frontend_ASG.Security_Group_ID]
 }
 
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
@@ -9,32 +9,32 @@ output "Security_Group_ID" {
 
 # Priavte Key
 output "key_pair_name" {
-  value       = [module.ASG.key_pair_name]
+  value       = [module.QA_Frontend_ASG.key_pair_name]
 }
 
 # Template
 output "launch_template_id" {
-  value = [module.ASG.launch_template_id]
+  value = [module.QA_Frontend_ASG.launch_template_id]
 }
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
 #--------------------------------- Target Group -----------------------------------#
 
 output "Target_group_id" {
-  value = [module.ASG.Target_group_id]
+  value = [module.QA_Frontend_ASG.Target_group_id]
 }
 
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
 #--------------------------Configure Auto Scaling group ---------------------------#
 
 output "Autoscaling_group_id" {
-  value = [module.ASG.Autoscaling_group_id]
+  value = [module.QA_Frontend_ASG.Autoscaling_group_id]
 }
 
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
 #---------------------------- Auto Scaling Policies -------------------------------#
 
 output "Autoscaling_policy_name" {
-  value       = [module.ASG.Autoscaling_policy_name]
+  value       = [module.QA_Frontend_ASG.Autoscaling_policy_name]
 }
 
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
