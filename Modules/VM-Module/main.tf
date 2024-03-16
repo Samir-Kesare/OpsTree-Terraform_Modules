b0833bf29a8624448687a6c0e75c8ef95c9356dd
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-03-16 15:36:57 +0530

    Update main.tf

--- Modules/VM-Module/main.tf
+++ Modules/VM-Module/main.tf
@@ -4,30 +4,39 @@ locals {
   inbound_ports         = var.inbound_ports
   outbound_ports        = var.outbound_ports
 }
+# Security Group #
+locals {
+  inbound_ports  = var.inbound_ports
+  outbound_ports = var.outbound_ports
+}
+
 resource "aws_security_group" "sec_grp" {
-  name                  = var.sec_grp_name
-  description           = var.Sec_grp_description
-  vpc_id                = var.vpc_id
+  name        = var.sec_grp_name
+  description = var.Sec_grp_description
+  vpc_id      = var.vpc_id
 
   dynamic "ingress" {
-  for_each              = local.inbound_ports
-  content {
-   from_port            = ingress.value.port
-   to_port              = ingress.value.port
-   protocol             = ingress.value.protocol
-   cidr_blocks          = [ingress.value.cidr_blocks]
+    for_each = local.inbound_ports
+    content {
+      from_port       = ingress.value.port
+      to_port         = ingress.value.port
+      protocol        = ingress.value.protocol
+      cidr_blocks      = contains(keys(ingress.value), "cidr_blocks") ? [ingress.value.cidr_blocks] : null
+      security_groups  = contains(keys(ingress.value), "security_group_ids") ? [ingress.value.security_group_ids] : null
+    }
   }
- }
- dynamic "egress" {
-  for_each              = local.outbound_ports
-  content {
-   from_port            = egress.value.port
-   to_port              = egress.value.port
-   protocol             = egress.value.protocol
-   cidr_blocks          = [egress.value.cidr_blocks]
+
+  dynamic "egress" {
+    for_each = local.outbound_ports
+    content {
+      from_port   = egress.value.port
+      to_port     = egress.value.port
+      protocol    = egress.value.protocol
+      cidr_blocks = [egress.value.cidr_blocks]
+    }
   }
- }
-  tags                  = var.Sec_grp_tags
+
+  tags = var.Sec_grp_tags
 }
 
 /*------------Genrate-Key--------------*/
