15034cf53ac02591fcc55248c89f2784d64142c9
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-01 18:51:47 +0530

    Update script.sh

--- wrapperCode/Frontend_Wrapper_Code/Dev_Env/script.sh
+++ wrapperCode/Frontend_Wrapper_Code/Dev_Env/script.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-ALB_DNS="Dev-ALB-394440977.ap-northeast-1.elb.amazonaws.com"  # Change the DNS give actual DEV-ALB DNS
+alb_dns="dev-alb-228261188.us-east-1.elb.amazonaws.com"  # Change the DNS give actual DEV-ALB DNS
 # Modify package.json with the ALB DNS placeholder
-sed -i 's/"homepage": "https:\/\/opstree.github.io",/"homepage": "http:\/\/${ALB_DNS}",/' /home/ubuntu/Frontend/package.json
+sed -i 's/"homepage": "https:\/\/opstree.github.io",/"homepage": "http:\/\/${alb_dns}",/' /home/ubuntu/Frontend/package.json
 sed -i '/"proxy": "http:\/\/localhost:3000"/d' /home/ubuntu/Frontend/package.json
 
+## Modify src directory
+sed -i "s|http://34.72.54.44:8080|$new_url|g" /home/ubuntu/Frontend/src/ListSalary.js
+
+sed -i "s|http://16.171.175.47:8080|$new_url|g" /home/ubuntu/Frontend/src/AttendanceForm.js /home/ubuntu/Frontend/src/AttendanceList.js
+
+sed -i "s|http://35.193.0.217:8080|$new_url|g" /home/ubuntu/Frontend/src/EmployeeData.js
+
+sed -i "s|http://34.72.54.44/:8080|$new_url|g" /home/ubuntu/Frontend/src/EmployeeForm.js /home/ubuntu/Frontend/src/EmployeeList.js
+
+
 # Start your application
-cd /home/ubuntu/Frontend 
-npm start
+export NODE_OPTIONS="--max-old-space-size=512"
+
+cd /home/ubuntu/Frontend && npm install
+
+cd /home/ubuntu/Frontend && npm start
