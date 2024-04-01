57e2ef9772a679a81f4b656313732fc32e105e9e
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-04-01 11:55:33 +0530

    Update variables.tf

--- Modules/VM-Module/variables.tf
+++ Modules/VM-Module/variables.tf
@@ -1,40 +1,44 @@
 variable "sec_grp_name" {
-  description     = "Name for the security group"
-  type            = string
-  default         = "dev_sg"
+  description = "Name for the security group"
+  type        = string
+  default     = "dev_sg"
 }
+
 variable "Sec_grp_description" {
-  description     = "Description for the security group"
-  type            = string
-  default         = "Security group for Dev Env"
+  description = "Description for the security group"
+  type        = string
+  default     = "Security group for Dev Env"
 }
+
 variable "vpc_id" {
-  description     = "ID of the VPC for instances"
-  type            = string
+  description = "ID of the VPC for instances"
+  type        = string
 }
 
 variable "inbound_ports" {
-  description     = "List of inbound ports, protocols and cidr block"
-  type            = list(map(any))
-  default         = [
-    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, 
+  description = "List of inbound ports, protocols, and CIDR blocks or security group IDs"
+  type        = list(map(any))
+  default     = [
+    { port = 22, protocol = "TCP", cidr_blocks = "20.0.0.0/28" }, 
     { port = 22, protocol = "TCP", security_group_ids = "sg-051aa66773d7b86c9" },
   ]
 }
 
 variable "outbound_ports" {
-  description     = "List of outbound ports, protocols and Cidr block "
-  type            = list(map(any))
-  default         = [
-    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
+  description = "List of outbound ports, protocols, and CIDR block"
+  type        = list(map(any)) // Ensure cidr_blocks is a list of strings
+  default     = [
+    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0" },
   ]
 }
+
 variable "Sec_grp_tags" {
-  type            = map(string)
-  default         = {
-    Name          = "dev-sg"
-    Enviroment    = "dev"
-    Owner         = "shreya"
+  description = "Tags for the security group"
+  type        = map(string)
+  default     = {
+    Name        = "dev_sg"
+    Environment = "dev"
+    Owner       = "shreya"
   }
 }
 
@@ -44,16 +48,22 @@ variable "key_name" {
 }
 
 variable "server_type" {
-  description = "Instance type for Standalone server"
+  description = "Instance type for standalone server"
   type        = string
 }
 
 variable "subnet_id" {
-  description = "ID of the subnet for the Standalone server"
+  description = "ID of the subnet for the standalone server"
   type        = string
 }
 
 variable "server_name" {
   description = "Name tag for the standalone server"
   type        = string
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1  
 }
