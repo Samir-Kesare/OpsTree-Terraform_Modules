619d09b3416557b28beebc1bb452895ba6e7a21d
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-04-01 11:54:39 +0530

    Update output.tf

--- Modules/VM-Module/output.tf
+++ Modules/VM-Module/output.tf
@@ -2,5 +2,5 @@ output "Security_Group_ID" {
   value = [aws_security_group.sec_grp.id]
 }
 output "server_id" {
-  value = [aws_instance.standalone_server.id]
+  value = [aws_instance.standalone_server[*].id]
 }
