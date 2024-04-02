5392ab70643d6ad4654110bca18ca46d369d37de
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-04-02 18:58:28 +0530

    Update output.tf

--- Modules/Network_Skeleton_Module/output.tf
+++ Modules/Network_Skeleton_Module/output.tf
@@ -11,31 +11,31 @@ output "private-subnets-id" {
   value = aws_subnet.dev_private_subnets.*.id
 }
 
-output "dev-igw-id" {
+output "igw-id" {
   value = aws_internet_gateway.dev_igw.id
 }
 
-output "dev-nat-id" {
+output "nat-id" {
   value = aws_nat_gateway.dev_nat.id
 }
 
-output "dev-public-RTB-id" {
+output "public-RTB-id" {
   value = aws_route_table.dev_public_rtb.id
 }
 
-output "dev-private-RTB-id" {
+output "private-RTB-id" {
   value = aws_route_table.dev_private_rtb.id
 }
 
-output "dev-frontend-nacl-id" {
+output "frontend-nacl-id" {
   value = aws_network_acl.dev_frontend_nacl.id
 }
 
-output "dev-backend-nacl-id" {
+output "backend-nacl-id" {
   value = aws_network_acl.dev_backend_nacl.id
 }
 
-output "dev-db-nacl-id" {
+output "db-nacl-id" {
   value = aws_network_acl.dev_db_nacl.id
 }
 
@@ -43,13 +43,17 @@ output "alb-sg-id" {
   value = aws_security_group.dev_alb_sg.id  
 }
 
-output "dev-alb-dns" {
+output "alb-dns" {
   value = aws_lb.dev_alb.dns_name
 }
 
-output "dev-route53-zone" {
+output "alb-listener-arn" {
+  value = aws_lb_listener.dev_alb_listener.arn
+}
+
+output "route53-zone" {
   value = aws_route53_zone.dev_route53_zone.name_servers
 }
-output "dev-route53-record" {
+output "route53-record" {
   value = aws_route53_record.dev_route53_record.name
 }
