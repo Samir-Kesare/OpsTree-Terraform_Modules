9e54894505e368e8df017c45dcc56753e8ac7555
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2024-04-07 20:57:53 +0530

    updated output of TG arn

--- Modules/target-group/output.tf
+++ Modules/target-group/output.tf
@@ -1,5 +1,5 @@
 #--------------------------------- Target Group -----------------------------------#
 
 output "target_group_id" {
-  value = [aws_lb_target_group.target-group.id]
+  value = aws_lb_target_group.target-group.arn
 }
