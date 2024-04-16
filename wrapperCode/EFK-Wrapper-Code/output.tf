f126dc7cc9a2b968ab5ee4f79885d8ec7a9bc4c7
aakashtripathi-snaatak <156056344+aakashtripathi-snaatak@users.noreply.github.com>
2024-04-16 08:55:49 +0530

    Update output.tf

--- wrapperCode/EFK-Wrapper-Code/output.tf
+++ wrapperCode/EFK-Wrapper-Code/output.tf
@@ -1,6 +1,6 @@
 output "Security_Group_ID" {
-  value = module.redis.Security_Group_ID
+  value = module.efk.Security_Group_ID
 }
 output "server_id" {
-  value = module.redis.server_id
+  value = module.efk.server_id
 }
