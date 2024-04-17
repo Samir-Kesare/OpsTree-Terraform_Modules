6e7341047547b3e85fbcbd0088b80ab50eab06f1
code-shantanu <156056364+code-shantanu@users.noreply.github.com>
2024-04-17 07:47:08 +0530

    Update output.tf

--- wrapperCode/PGA-Wrapper-Code/output.tf
+++ wrapperCode/PGA-Wrapper-Code/output.tf
@@ -1,6 +1,6 @@
 output "Security_Group_ID" {
-  value = module.redis.Security_Group_ID
+  value = module.pga.Security_Group_ID
 }
 output "server_id" {
-  value = module.redis.server_id
+  value = module.pga.server_id
 }
