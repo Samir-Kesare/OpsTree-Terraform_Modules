a69711a69a0372a22a2754dd2349c860814a6d6f
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-04-24 14:24:48 +0530

    Update output.tf

--- wrapperCode/Attendance-API/Dev/output.tf
+++ wrapperCode/Attendance-API/Dev/output.tf
@@ -1,34 +1,40 @@
-# Security group
+#---------------------------------Security Group ----------------------------------#
 
 output "Security_Group_ID" {
   value = [module.Dev_Attendance.Security_Group_ID]
 }
 
-# Private Key
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
+#--------------------------------Launch Template ----------------------------------#
+
+# Priavte Key
 output "key_pair_name" {
-  value = [module.Dev_Attendance.key_pair_name]
+  value       = [module.Dev_Attendance.key_pair_name]
 }
 
 # Template
-
 output "launch_template_id" {
   value = [module.Dev_Attendance.launch_template_id]
 }
-
-#Target group id
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
+#--------------------------------- Target Group -----------------------------------#
 
 output "Target_group_id" {
   value = [module.Dev_Attendance.Target_group_id]
 }
 
-# ASG
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
+#--------------------------Configure Auto Scaling group ---------------------------#
+
 output "Autoscaling_group_id" {
   value = [module.Dev_Attendance.Autoscaling_group_id]
 }
 
-# ASG Policies
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
+#---------------------------- Auto Scaling Policies -------------------------------#
 
 output "Autoscaling_policy_name" {
-  value = [module.Dev_Attendance.Autoscaling_policy_name]
+  value       = [module.Dev_Attendance.Autoscaling_policy_name]
 }
 
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
