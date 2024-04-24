f33b7eb490c7e642fbd5604575078a91dabb8b5a
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-04-24 14:30:09 +0530

    Update script.sh

--- wrapperCode/Attendance-API/Dev/script.sh
+++ wrapperCode/Attendance-API/Dev/script.sh
@@ -5,6 +5,7 @@ sed -i 's/10\.0\.0\.109/10.0.0.7/g' /home/ubuntu/OT-attendance/config.yaml   # R
 sed -i 's/10\.0\.0\.8/10.0.0.4/g'  /home/ubuntu/OT-attendance/config.yaml   # Postgress
 sed -i 's/p@ssw0rd/password/g'  /home/ubuntu/OT-attendance/config.yaml  # Password changes
 
-cd /home/ubuntu/OT-attendance 
+cd /home/ubuntu/OT-attendance
+sudo apt update
 make run-migrations 
 sudo systemctl restart attendance.service 
