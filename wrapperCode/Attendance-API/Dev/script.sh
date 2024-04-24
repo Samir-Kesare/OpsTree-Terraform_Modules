d2a218f940cd7bba76a12101197a0972d5656571
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-04-24 14:16:58 +0530

    Update script.sh

--- wrapperCode/Attendance-API/Dev/script.sh
+++ wrapperCode/Attendance-API/Dev/script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd attendance
-cd attendance-api/
-poetry shell
-gunicorn app:app --log-config log.conf -b 0.0.0.0:8080
+
+sed -i 's/10\.0\.0\.8/10.0.0.4/g' /home/ubuntu/OT-attendance/liquibase.properties  # Postgress
+sed -i 's/10\.0\.0\.109/10.0.0.7/g' /home/ubuntu/OT-attendance/config.yaml   # Redis
+sed -i 's/10\.0\.0\.8/10.0.0.4/g'  /home/ubuntu/OT-attendance/config.yaml   # Postgress
+sed -i 's/p@ssw0rd/password/g'  /home/ubuntu/OT-attendance/config.yaml  # Password changes
+
+cd /home/ubuntu/OT-attendance 
+make run-migrations 
+sudo systemctl restart attendance.service 
